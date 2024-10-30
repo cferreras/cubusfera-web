11b115da098ceb02808e34bd5aacf6309b59dcd7
Carlos Ferreras <carlos@carlosferreras.com>
2024-10-31 00:06:55 +0100

    fix: update eslint config and remove unused import in mapa page

--- src/app/mapa/page.tsx
+++ src/app/mapa/page.tsx
@@ -1,5 +1,4 @@
 import Container from "@/components/Container";
-import Rules from "@/components/Rules";
 import Title from "@/components/Title";
 import {ArrowUpRightFromSquare} from "lucide-react";
 
