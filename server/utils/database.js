58778a6034859bb837f1da60e53fadec2e696229
Carlos Ferreras <carlos@carlosferreras.com>
2024-10-31 01:34:52 +0100

    fix: explicit include mysql2

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,9 +1,11 @@
+import mysql2 from 'mysql2';
 import { Sequelize } from "sequelize";
 
 import 'dotenv/config'
 
 const sequelize = new Sequelize(process.env.database, process.env.database_user, process.env.database_password, {
   dialect: "mysql",
+  dialectModule: mysql2,
   host: process.env.database_host,
   port: process.env.database_port,
   charset: "utf8mb4",
