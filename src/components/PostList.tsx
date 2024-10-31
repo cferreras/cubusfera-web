d58f120f8388e90c36ed36f9b44950738589e193
Carlos Ferreras <carlos@carlosferreras.com>
2024-10-31 01:48:26 +0100

    chore: eslint

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale'
 
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export default function PostList({ posts }: { posts: any[] }) {
     return (
         <>
