9a83bf36053eb7350136f2aaf96582c6bc6c0531
Carlos Ferreras <carlos@carlosferreras.com>
2025-03-20 13:30:15 +0100

    fix hydration error

--- components/DarkmodeToggle.tsx
+++ components/DarkmodeToggle.tsx
@@ -6,6 +6,12 @@ import { useTheme } from "next-themes"
 
 export default function DarkmodeToggle() {
     const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = React.useState(false)
+
+    React.useEffect(() => {
+        setMounted(true)
+    }, [])
+
     const cycleTheme = () => {
         if (theme === 'system') {
             setTheme('dark')
@@ -14,6 +20,16 @@ export default function DarkmodeToggle() {
         } else {
             setTheme('system')
         }
+    }
+
+    if (!mounted) {
+        return (
+            <button className="flex items-center gap-4 px-4 py-5 bg-neutral-100 dark:hover:bg-neutral-700 dark:focus-visible:bg-neutral-700 hover:bg-neutral-200 focus-visible:bg-neutral-300 border border-neutral-200 dark:border-neutral-700 dark:bg-neutral-800 rounded-2xl outline-none">
+                <div className="relative w-[1.2rem] h-[1.2rem]"></div>
+                <div></div>
+                <span className="sr-only">Cambiar tema</span>
+            </button>
+        )
     }
 
     return (
