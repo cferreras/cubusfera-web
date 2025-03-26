fec32a324a7ab7409e19ab4fb4362ff8780b474c
Carlos Ferreras <carlos@carlosferreras.com>
2025-03-26 14:18:19 +0100

    style: remove bold styling from "Legal" heading
    The "Legal" heading no longer requires bold styling to maintain consistency with the design guidelines. This change improves visual hierarchy and alignment with the overall UI aesthetic.

--- app/(Cubusfera)/legal/page.tsx
+++ app/(Cubusfera)/legal/page.tsx
@@ -12,7 +12,7 @@ export default function Normas() {
         <>
             <Container className="py-20">
                 <div className="flex flex-col gap-1">
-                    <div className="text-lg font-bold">Legal</div>
+                    <div className="text-lg">Legal</div>
                     <div className="flex flex-col divide-y-2 dark:divide-neutral-900 divide-white text-md mt-4"> 
                         <Link className="p-6 dark:bg-neutral-800 bg-neutral-100 rounded-t-3xl dark:hover:bg-neutral-700 hover:bg-neutral-200" href="/legal/privacidad">Política de privacidad</Link>
                         <Link className="p-6 dark:bg-neutral-800 bg-neutral-100 rounded-b-3xl dark:hover:bg-neutral-700 hover:bg-neutral-200" href="/legal/terminos">Términos de servicio</Link>
